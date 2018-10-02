309d4fa3dc536611ce7842d5213bb05ad519db50
Ricardo <Ricardo@192.168.1.5>
2018-10-02 17:44:42 -0300

    commit

--- public_html/Aula06/CalculaArea/javascript.js
+++ public_html/Aula06/CalculaArea/javascript.js
@@ -2,8 +2,8 @@ var nome = window.prompt("Login:");
 var senha = window.prompt("Senha:");
 
 if(nome == "Ricardo" && senha == "123"){
-    window.location.assign();
+    window.location.assign("PaginaPrincipal.html");
     
 }else {
     alert("Acesso negado");
-}
+   }
